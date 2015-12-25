b5e527cc69d4d3557476519d4a2ebf7af870ca6d
Guro Bokum <jiojiajiu@gmail.com>
2015-12-25 11:02:53 +0300

    Extend errors

--- backend/modules/error.js
+++ backend/modules/error.js
@@ -1,11 +1,18 @@
 var config = require('config');
+var utils = require('./utils');
 
 function ServerError (message, code, params) {
     this.message = message || 'Empty error';
     this.code = code || config.errors.EMPTY;
     this.params = params || {};
+
+    this.toString = function ServerError_toString () {
+        return this.message;
+    }
+
+    this.stack = ((new Error(this.message)).stack);
 }
 
-ServerError.prototype = new Error();
+ServerError.prototype = Error.prototype;
 
 module.exports = ServerError;
---
+++ backend/modules/utils.js
@@ -0,0 +1,7 @@
+var utils = {
+    concat: function utils_concat () {
+        return Array.prototype.join.call(arguments, ' ');
+    }
+}
+
+module.exports = utils;
